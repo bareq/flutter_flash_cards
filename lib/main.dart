d353835f6127fe518f0fbe1cca5d8459157bef2c
Bartosz Lach <barqlast@gmail.com>
2023-09-08 12:25:49 +0200

    Extracted answer button widget

--- lib/main.dart
+++ lib/main.dart
@@ -46,32 +46,46 @@ class MyHomePage extends StatelessWidget {
         children: [
           Flexible(child: FlashCardWidget()),
           if (flashCardViewModel.cardFlipped)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.greenAccent,
-                  ),
-                  onPressed: () {
-                    flashCardViewModel.nextCard();
-                  },
-                  child: const Text('Znałem'),
-                ),
-                const SizedBox(width: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.redAccent,
-                  ),
-                  onPressed: () {
-                    flashCardViewModel.nextCard();
-                  },
-                  child: const Text('Nie znałem'),
-                ),
-              ],
-            )
+            AnswerButtons(flashCardViewModel: flashCardViewModel)
         ],
       )),
+    );
+  }
+}
+
+class AnswerButtons extends StatelessWidget {
+  const AnswerButtons({
+    super.key,
+    required this.flashCardViewModel,
+  });
+
+  final FlashCardViewModel flashCardViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent,
+          ),
+          onPressed: () {
+            flashCardViewModel.nextCard();
+          },
+          child: const Text('Znałem'),
+        ),
+        const SizedBox(width: 16),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.redAccent,
+          ),
+          onPressed: () {
+            flashCardViewModel.nextCard();
+          },
+          child: const Text('Nie znałem'),
+        ),
+      ],
     );
   }
 }
